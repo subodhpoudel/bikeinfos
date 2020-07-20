cafac6a9c74b23ff19e9b9fdc859df7b648c3743
subodhpoudel <subodhcj@gmail.com>
2020-07-20 19:58:29 +0545

    honda products

---
+++ honda/honda.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+site = input("Enter the url of the site that you want to scrape: ")
+page = requests.get(site, verify=False)
+name = input("Enter the bike: ")
+soup = BeautifulSoup(page.content, 'html.parser')
+#specs = soup.find_all(class_='tech-specs collapse-ul')
+specs = soup.select('li > small')
+details = soup.select('li > p')
+small = [spec.get_text() for spec in specs] 
+x = 0
+while x < 3:
+    rem = small.pop(0)
+    x = x + 1
+para = [spec.get_text() for spec in details]
+x = len(para)
+rem = para.pop(x-1)
+#specs = soup.select('.specs-detail')
+#for i in specs:
+    #print([i.text])
+    
+print(small)
+print(para)
+bike = pd.DataFrame(
+    {'Specs': small,
+     'Details': para,
+    })
+
+print(bike)
+bike.to_csv(name +'.csv')
