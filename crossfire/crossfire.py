1c1742c22a7af5f192fa0ffeb8e319765d9ff293
subodhpoudel <subodhcj@gmail.com>
2020-07-31 03:12:43 +0545

    complete crossfire bikes info

--- crossfire/crossfire.py
+++ crossfire/crossfire.py
@@ -6,9 +6,12 @@ site = input("Enter the url of the site that you want to scrape: ")
 page = requests.get(site)
 name = input("Enter the bike: ")
 soup = BeautifulSoup(page.content, 'html.parser')
-specs = soup.find_all(class_='specs-detail')
-small = [spec.find('small').get_text() for spec in specs] 
-para = [spec.find('p').get_text() for spec in specs]
+specs = soup.select('.specs-detail > li')
+price = ['Price']
+small = price + [spec.find('small').get_text() for spec in specs] 
+bike_price = soup.select('.wpb_wrapper > h2')
+cost = bike_price[1].get_text()
+para = [cost] + [spec.find('p').get_text() for spec in specs]
 #specs = soup.select('.specs-detail')
 #for i in specs:
     #print([i.text])
